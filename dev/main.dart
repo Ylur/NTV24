49ce0d34aaa1d4fcc5c2010906dcbdece89f05fb
Ylur <158998739+Ylur@users.noreply.github.com>
2024-03-08 09:19:19 +0000

    Add files via upload

---
+++ dev/main.dart
@@ -0,0 +1,18 @@
+import 'package:untitled/methods.dart' as methods;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  String name = 'Steindór';
+  String msg = 'would you like to learn Dart strings today?';
+
+  print(name + ', ' + msg);
+
+  String author = 'Albert Einstein';
+  String quote = "A person who never made a mistake never tried anything new.'";
+
+  print(author + ' once said: ' + quote);
+
+  String message = 'Hello World';
+  print(message);
+  // String message = 'Goodbye World';
+}
