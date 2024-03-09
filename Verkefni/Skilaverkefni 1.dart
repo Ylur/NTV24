b2ea33d447740b1077e51bad0e74624529ae4c8e
Ylur <158998739+Ylur@users.noreply.github.com>
2024-03-09 14:20:49 +0000

    Update Skilaverkefni 1.dart

--- Verkefni/Skilaverkefni 1.dart
+++ Verkefni/Skilaverkefni 1.dart
@@ -1,42 +1,29 @@
-import "dart:convert";
-import "dart:io";
-import "dart:math";
-
-import "package:untitled/methods.dart";
-import '../cheatSheet/cheatsheet_06.dart';
-
+import "dart:io"
+  
 void main(){
+  //1. Prints Hello and your name in a seperate line.
+   print("Hello.Ingi");
 
-/*
-List<String> names = getNamesList();
-Random random = Random();
-for (String name in names) {
-int randomNumber = random.nextInt(100);
-String message = greeting(name, randomNumber);
-print(message);
+  //2. Prints the sum of two numbers
+  int num1 = 10;
+  int num2 = 20;
+  int sum = num1 + num2;
+  print("The sum of $num1 and $num2 is $sum");
 
-}
+  //3. Prints the result of dividing two numbers
+  int numb1 = 50;
+  int numb2 = 10;
+  int result = numb1 ~/ numb2;
+  print("The result of dividing $numb1 by $numb2 is $result");
 
- */
-
-List<Customer> myCustomers = [];
-Customer myCustomer = Customer("Ingi", 35);
-myCustomers.add(myCustomer);
-for(Customer c in getCustomers()){
-  myCustomers.add(c);
-}
-print(myCustomers);
-print("_______________");
-
-myCustomers = getCustomers();
-
-print(myCustomer.toString());
-
-
-
-
-
-
-
+  //4. Prints the result of the specified operation
+  //4.1
+  print(-1 + 5 * 5);
+  //4.2
+  print((25 + 5) % 4);
+  //4.3
+  print(8 + 6 * 4 ~/ 2);
+  //4.4
+  print(2 + 15 / 6 * 1 - 7 % 2);
 
 }
